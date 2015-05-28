0298c70920e545df860e69ceb77104cfe07c0642
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-28 13:54:17 +0200

    Create template classes

--- Editor/TodoEditor.cs
+++ Editor/TodoEditor.cs
@@ -1,15 +1,71 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEditor;
+using UnityEngine;
 
-public class TodoEditor : MonoBehaviour {
+namespace AnnotationEx {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [CustomEditor(typeof(Todo))]
+    [CanEditMultipleObjects]
+    public sealed class TodoEditor : Editor {
+        #region FIELDS
+
+        private Todo Script { get; set; }
+
+        #endregion FIELDS
+
+        #region SERIALIZED PROPERTIES
+
+        private SerializedProperty description;
+
+        #endregion SERIALIZED PROPERTIES
+
+        #region UNITY MESSAGES
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            DrawVersionLabel();
+            DrawDescriptionTextArea();
+
+            EditorGUILayout.Space();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void OnEnable() {
+            Script = (Todo)target;
+
+            description = serializedObject.FindProperty("description");
+        }
+
+        #endregion UNITY MESSAGES
+
+        #region INSPECTOR CONTROLS
+
+        private void DrawVersionLabel() {
+            EditorGUILayout.LabelField(
+                string.Format(
+                    "{0} ({1})",
+                    Todo.Version,
+                    Todo.Extension));
+        }
+
+        private void DrawDescriptionTextArea() {
+            description.stringValue = EditorGUILayout.TextArea(
+                description.stringValue);
+        }
+
+        #endregion INSPECTOR
+
+        #region METHODS
+
+        [MenuItem("Component/Annotation/Todo")]
+        private static void AddEntryToComponentMenu() {
+            if (Selection.activeGameObject != null) {
+                Selection.activeGameObject.AddComponent(typeof(Todo));
+            }
+        }
+
+        #endregion METHODS
+    }
+
 }
--- Todo.cs
+++ Todo.cs
@@ -1,15 +1,90 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Todo : MonoBehaviour {
+namespace AnnotationEx {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public sealed class Todo : MonoBehaviour {
+
+        #region CONSTANTS
+
+        public const string Version = "v0.1.0";
+        public const string Extension = "Annotation";
+
+        #endregion
+
+        #region DELEGATES
+        #endregion
+
+        #region EVENTS
+        #endregion
+
+        #region FIELDS
+
+#pragma warning disable 0414
+        /// <summary>
+        ///     Allows identify component in the scene file when reading it with
+        ///     text editor.
+        /// </summary>
+        [SerializeField]
+        private string componentName = "Todo";
+#pragma warning restore0414
+
+        #endregion
+
+        #region INSPECTOR FIELDS
+
+        [SerializeField]
+        private string description = "Description";
+
+        #endregion
+
+        #region PROPERTIES
+
+        /// <summary>
+        ///     Optional text to describe purpose of this instance of the component.
+        /// </summary>
+        public string Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        #endregion
+
+        #region UNITY MESSAGES
+
+        private void Awake() { }
+
+        private void FixedUpdate() { }
+
+        private void LateUpdate() { }
+
+        private void OnEnable() { }
+
+        private void Reset() { }
+
+        private void Start() { }
+
+        private void Update() { }
+
+        private void OnValidate() { }
+
+        private void OnCollisionEnter(Collision collision) { }
+
+        private void OnCollisionStay(Collision collision) { }
+
+        private void OnCollisionExit(Collision collision) { }
+
+        #endregion
+
+        #region EVENT INVOCATORS
+        #endregion
+
+        #region EVENT HANDLERS
+        #endregion
+
+        #region METHODS
+        #endregion
+
+    }
+
 }
